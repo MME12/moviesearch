8de22424191715c970e19bddd7d91996bf657d90
MMEDU <manemanuk5@gmail.com>
2018-05-15 11:11:04 +0400

    Corrected design and added local storage

--- src/app/modules/home/components/list/selected-list.component.ts
+++ src/app/modules/home/components/list/selected-list.component.ts
@@ -1,32 +1,46 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, Output,  EventEmitter } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'selected-list',
   templateUrl: './selected-list.component.html'
 })
-export class SelectedListComponent  {
+export class SelectedListComponent   implements OnInit {
   @Input() movieToAdd: any;
+  @Output() onSelect = new EventEmitter<string>();
 	list: Array<any> = [];
-  constructor() { }
+  shown: boolean = false;
+  constructor(private movieService: MovieService) { }
 
   ngOnChanges() {
     console.log('ngOnChanges')
-    
     this.addToList ();
   }
 
+  ngOnInit() {
+      this.getList();
+   }
+  toggleList () {
+    this.shown = !this.shown;
+  }
+  getList () {
+    this.list = this.movieService.getSelectedMoviesFromLocal();
+  }
   addToList () {
     if(!this.movieToAdd) {
       return;
     }
-    let item = this.list.find( value => value.id == this.movieToAdd.id);
-    if(!item) {
-      this.list.push(this.movieToAdd)
-    }
+    this.movieService.addToSelectedMovies(this.movieToAdd);
+    this.list = this.movieService.getSelectedMoviesFromLocal();
   }
   removeFromList (id: string) {
-  	this.list = this.list.filter(movie => {
+  	/*this.list = this.list.filter(movie => {
       return movie.id != id;
-    })
+    })*/
+    this.movieService.removeFromSelectedMovies(id);
+    this.list = this.movieService.getSelectedMoviesFromLocal();
+  }
+  open(id: string) {
+      this.onSelect.emit(id);
   }
 }
--- src/app/modules/home/services/movie.service.ts
+++ src/app/modules/home/services/movie.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+class SelectedMovie {
+	constructor(public Title: string = '', public id: string = '') {}
+}
+
 @Injectable()
 export class MovieService {
 	//apikey=1e2ef0e9
@@ -15,5 +19,30 @@ export class MovieService {
   }
   getMovie(id) {
 	  return this.http.get(this.detailUrl + id);
+  }
+  addToSelectedMovies (movie: SelectedMovie) {
+    if(!movie) {
+      return;
+    }
+    let selectedMovies = this.getSelectedMoviesFromLocal ();
+    let item = selectedMovies.find( value => value.id == movie.id);
+    if(!item) {
+      selectedMovies.push(movie);
+      this.setSelectedMoviesFromLocal (selectedMovies);
+    }
+  }
+  removeFromSelectedMovies (id: string) {
+  	let selectedMovies = this.getSelectedMoviesFromLocal ();
+  	selectedMovies = selectedMovies.filter(movie => {
+      return movie.id != id;
+    })
+    this.setSelectedMoviesFromLocal (selectedMovies);
+  }
+  getSelectedMoviesFromLocal (): SelectedMovie[] {
+  	let localStorageItem = JSON.parse(localStorage.getItem('selectedMovies'));
+  	return localStorageItem ? localStorageItem.selectedMovies : []
+  }
+  setSelectedMoviesFromLocal (selectedMovies: SelectedMovie[]): void {
+  	localStorage.setItem('selectedMovies', JSON.stringify({selectedMovies: selectedMovies}))
   }
 }
